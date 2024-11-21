e087bf468719a10172f04aa91e7bf93811b672d6
Renee Rudderow <rrudderow@yahoo.com>
2024-11-21 15:58:46 -0700

    type annotations

--- final_project/item.py
+++ final_project/item.py
@@ -4,40 +4,37 @@ from datetime import date
 
 
 class Item:
-    def __init__(self, name, item_id, warehouse=None):
+    def __init__(self, name: str, item_id: int, warehouse: str) -> None:
         """Item Constructor"""
         self._name = name
         self._item_id = item_id
         self._warehouse = warehouse
-
-        # Automatically associate this item with the given warehouse
-        if warehouse:
-            warehouse.add_item(self)
+        warehouse.add_item(self)
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @name.setter
-    def name(self, name):
+    def name(self, name: str) -> None:
         self._name = name
 
     @property
-    def item_id(self):
+    def item_id(self) -> int:
         return self._item_id
 
     @item_id.setter
-    def item_id(self, item_id):
+    def item_id(self, item_id: int) -> None:
         self._item_id = item_id
 
     @property
-    def warehouse(self):
+    def warehouse(self) -> str:
         return self._warehouse
 
     @warehouse.setter
-    def warehouse(self, warehouse):
+    def warehouse(self, warehouse: str) -> None:
         self._warehouse = warehouse
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Return a string representation of the item for printing"""
         return f"Item {self._name} (ID: {self._item_id})"
--- final_project/perishables.py
+++ final_project/perishables.py
@@ -2,7 +2,7 @@ from item import Item
 from datetime import date
 
 class Perishables(Item):
-    def __init__(self, name, item_id, expiration_date, warehouse=None):
+    def __init__(self, name: str, item_id: int, expiration_date: date, warehouse: str) -> None:
         """Perishable Constructor"""
         # Call the parent constructor
         super().__init__(name, item_id, warehouse)
@@ -11,19 +11,19 @@ class Perishables(Item):
         self._expiration_date = expiration_date
 
     @property
-    def expiration_date(self):
+    def expiration_date(self) -> date:
         return self._expiration_date
 
     @expiration_date.setter
-    def expiration_date(self, expiration_date):
+    def expiration_date(self, expiration_date) -> None:
         self._expiration_date = expiration_date
 
-    def is_expired(self):
+    def is_expired(self) -> bool:
         """Check if the item is expired by comparing the expiration date to today's date"""
         today = date.today()
         return self._expiration_date < today
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Override the string representation to include expiration date and warehouse"""
         warehouse_info = f", Warehouse: {self._warehouse}" if self._warehouse else ""
         return f"Perishable Item {self._name} (ID: {self._item_id}), Expiration Date: {self._expiration_date}{warehouse_info}"
