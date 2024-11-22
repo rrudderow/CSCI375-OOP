c8c93900473dcf5b06a279166a6327fcf08188ff
Cody Stoffel <stoffelcody15@gmail.com>
2024-11-21 18:06:59 -0700

    added warehouses and cli

---
+++ final_project/cli.py
@@ -0,0 +1,72 @@
+from typing import Optional
+from rich.console import Console
+from rich.table import Table
+import sys
+from warehouses import Warehouses
+from warehouse import Warehouse
+from item import Item
+
+class Cli:
+    
+    _instance: Optional["Cli"] = None
+
+    def __init__(self) -> None:
+        """ Constructor """
+        if Cli._instance:
+            raise NameError(
+                "Cannot create multiple instances of \
+                a singleton class Solution")
+
+        self.console = Console()
+
+    def nameWarehouses(self) -> None:
+        warehouses = Warehouses()
+        
+        name = "default"
+
+        while name != "":
+            
+            print("enter new to create a warehouse\n"
+                  "enter addItem to add an item to a warehouse\n"
+                  "enter moveItem to move an item to a new warehose\n" 
+                  "enter view to see warehouses\n")
+            
+            name = sys.stdin.readline()[:-1]
+            
+            if (name == "new"):
+                print("what do you want to name your warehouse")
+                whName = sys.stdin.readline()[:-1]
+                warehouses.createWarehouse(whName)
+            
+            if (name == "addItem"):
+                
+                print("give the item name, id then name of warehouse: ")
+
+                itemName = sys.stdin.readline()[:-1]
+                itemID = sys.stdin.readline()[:-1]
+                whName = sys.stdin.readline()[:-1]
+                
+                Item(itemName, itemID, warehouses.getWarehouse(whName))
+
+            if (name == "moveItem"):
+
+                print("give the name of the warehouse you want to move from: ")
+
+                moveWh = sys.stdin.readline()[:-1]
+
+                print("what is the name of the item you want to move: ")
+
+                itmName = sys.stdin.readline()[:-1]
+
+                print("what is the name of the warehouse you want to move it to: ")
+
+                other = sys.stdin.readline()[:-1]
+
+                warehouses.getWarehouse(moveWh).send_item(itmName, warehouses.getWarehouse(other))
+                
+            if (name == "view"):
+            
+                print("which warehouse would you like to see: ")
+                name = sys.stdin.readline()[:-1]
+
+                warehouses.getWarehouse(name)
---
+++ final_project/warehouses.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from warehouse import Warehouse
+
+class Warehouses:
+    
+    _instance: Optional["Warehouses"] = None
+
+    def __init__(self) -> None:
+        """ Constructor """
+        if Warehouses._instance:
+            raise NameError(
+                "Cannot create multiple instances of \
+                a singleton class Solution")
+
+        self._warehouses: Warehouse = []  # should be viable with subclasses of Item
+        Warehouses._instance = self
+
+    @classmethod
+    def get_instance(cls) -> Optional["Warehouses"]:
+        """Returns the instance of the class
+
+        Returns:
+            Solution: class instance
+        """
+        return cls._instance
+
+    @classmethod
+    def reset_instance(cls) -> None:
+        """Resets the instance
+        """
+        cls._instance = None
+
+    def createWarehouse(self, name) -> None :
+        
+        warehouse = Warehouse(name)
+        self._warehouses.append(warehouse)
+
+    
+    def getWarehouse(self, name):
+        for i in self._warehouses:
+            if name == i.name:
+                return i
