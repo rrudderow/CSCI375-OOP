8c8c29c9dccd5b727e6103200870a1887a48ea0b
Cody Stoffel <stoffelcody15@gmail.com>
2024-11-27 16:25:57 -0700

    finishing Cli and Warehouses

---
+++ final_project/tests/test_cli.py
@@ -0,0 +1,18 @@
+from unittest import mock
+from cli import Cli
+
+
+def test_run_cli() -> None:
+    """This tests the cli """
+
+    with (mock.patch('rich.console.Console.print') as new_console,
+            mock.patch('sys.stdin') as test_input):
+
+        test_input.readline.side_effect = ["1", "wh1", "0"]
+        Cli().run_cli()
+        new_console.assert_called()
+
+
+        test_input.readline.side_effect = ["2", "apple", "1", "wh1", "0"]
+        Cli().run_cli()
+        new_console.assert_called()
